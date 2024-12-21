b513f384c69626ef11a46a6fbf6ef0d17bdf49de
Anton Vorobev <afvorobev@gmail.com>
2024-12-21 15:48:21 +0300

    feat: controls updated

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import * as THREE from 'three';
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import { GUI } from 'three/addons/libs/lil-gui.module.min.js';
+import { PointerLockControls } from 'three/addons/controls/PointerLockControls.js';
 
 const POLLING_INTERVAL = 1000;
 const API_URL = 'http://localhost:8080/';
@@ -25,7 +25,7 @@ class MinMaxGUIHelper {
   }
   set max(v) {
     this.obj[this.maxProp] = v;
-    this.min = this.min; // this will call the min setter
+    this.min = this.min;
   }
 }
 
@@ -40,17 +40,104 @@ const getCube = (x, y, z, color = 'green', opacity = 0.3) => {
   return mesh;
 };
 
+let moveForward = false;
+let moveBackward = false;
+let moveLeft = false;
+let moveRight = false;
+let moveUp = false;
+let moveDown = false;
+let goFast = false;
+
+let prevTime = performance.now();
+const velocity = new THREE.Vector3();
+const direction = new THREE.Vector3();
+
 function main() {
   const renderer = new THREE.WebGLRenderer();
   renderer.setSize(window.innerWidth, window.innerHeight);
   document.body.appendChild(renderer.domElement);
 
   const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 500);
-  camera.position.set(-200, 200, -200);
+  camera.position.set(-100, 100, -100);
+  camera.lookAt(new THREE.Vector3(0, 90, 0));
 
-  const controls = new OrbitControls(camera, renderer.domElement);
-  controls.target.set(0, 5, 0);
-  controls.update();
+  const controls = new PointerLockControls(camera, document.body);
+
+  const onKeyDown = function (event) {
+    switch (event.code) {
+      case 'ArrowUp':
+      case 'KeyW':
+        moveForward = true;
+        break;
+
+      case 'ArrowLeft':
+      case 'KeyA':
+        moveLeft = true;
+        break;
+
+      case 'ArrowDown':
+      case 'KeyS':
+        moveBackward = true;
+        break;
+
+      case 'ArrowRight':
+      case 'KeyD':
+        moveRight = true;
+        break;
+
+      case 'Space':
+        moveUp = true;
+        break;
+
+      case 'KeyC':
+        moveDown = true;
+        break;
+
+      case 'ShiftLeft':
+        goFast = true;
+        break;
+    }
+  };
+
+  const onKeyUp = function (event) {
+    switch (event.code) {
+      case 'ArrowUp':
+      case 'KeyW':
+        moveForward = false;
+        break;
+
+      case 'ArrowLeft':
+      case 'KeyA':
+        moveLeft = false;
+        break;
+
+      case 'ArrowDown':
+      case 'KeyS':
+        moveBackward = false;
+        break;
+
+      case 'ArrowRight':
+      case 'KeyD':
+        moveRight = false;
+        break;
+
+      case 'Space':
+        moveUp = false;
+        break;
+
+      case 'KeyC':
+        moveDown = false;
+        break;
+
+      case 'ShiftLeft':
+        goFast = false;
+        break;
+    }
+  };
+
+  document.addEventListener('keydown', onKeyDown);
+  document.addEventListener('keyup', onKeyUp);
+  renderer.domElement.addEventListener('click', () => controls.lock());
 
   function updateCamera() {
     camera.updateProjectionMatrix();
@@ -58,7 +145,7 @@ function main() {
 
   const gui = new GUI();
   const minMaxGUIHelper = new MinMaxGUIHelper(camera, 'near', 'far', 0.1);
-  gui.add(minMaxGUIHelper, 'max', 0.1, 1000, 0.1).name('Distance').onChange(updateCamera);
+  gui.add(minMaxGUIHelper, 'max', 0.1, 1000, 0.1).name('View distance').onChange(updateCamera);
 
   const scene = new THREE.Scene();
   scene.background = new THREE.Color('white');
@@ -175,6 +262,7 @@ function main() {
     const state = await response.json();
     renderState(state);
   };
+  renderMock(0);
 
   document.getElementById('prev').onclick = async () => {
     if (isNaN(activeFileIndex) || !activeFileIndex) {
@@ -215,6 +303,32 @@ function main() {
   };
 
   function render() {
+    const time = performance.now();
+
+    if (controls.isLocked === true) {
+      const delta = Number(goFast) * 0.05 + (time - prevTime) / 1000;
+
+      velocity.x -= velocity.x * 10.0 * delta;
+      velocity.z -= velocity.z * 10.0 * delta;
+      velocity.y -= velocity.y * 10.0 * delta;
+
+      direction.z = Number(moveForward) - Number(moveBackward);
+      direction.x = Number(moveRight) - Number(moveLeft);
+      direction.y = Number(moveUp) - Number(moveDown);
+      direction.normalize(); // this ensures consistent movements in all directions
+
+      if (moveForward || moveBackward) velocity.z -= direction.z * 400.0 * delta;
+      if (moveLeft || moveRight) velocity.x -= direction.x * 400.0 * delta;
+      if (moveUp || moveDown) velocity.y -= direction.y * 400.0 * delta;
+
+      controls.moveRight(-velocity.x * delta);
+      controls.moveForward(-velocity.z * delta);
+
+      controls.object.position.y += -velocity.y * delta;
+    }
+
+    prevTime = time;
+
     renderer.render(scene, camera);
     requestAnimationFrame(render);
   }
