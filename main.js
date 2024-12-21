c97f113eb331c7b2ee181d8be281a35cd6129e9d
Anton Vorobev <afvorobev@gmail.com>
2024-12-21 08:07:09 +0300

    feat: three cubes

--- main.js
+++ main.js
@@ -34,15 +34,25 @@
 
 import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
-import { GUI } from 'three/addons/libs/lil-gui.module.min.js';
+
+const getCube = (x, y, z, color = 'green') => {
+  const cubeSize = 1;
+  const cubeGeo = new THREE.BoxGeometry(cubeSize, cubeSize, cubeSize);
+
+  const cubeMat = new THREE.MeshBasicMaterial({ color, transparent: true, opacity: 0.3 });
+  const mesh = new THREE.Mesh(cubeGeo, cubeMat);
+  mesh.position.set(x * cubeSize + cubeSize / 2, y * cubeSize + cubeSize / 2, z * cubeSize + cubeSize / 2);
+
+  return mesh;
+};
 
 function main() {
-  const renderer = new THREE.WebGLRenderer({ antialias: true });
+  const renderer = new THREE.WebGLRenderer();
   renderer.setSize(window.innerWidth, window.innerHeight);
   document.body.appendChild(renderer.domElement);
 
   const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-  camera.position.set(0, 10, 20);
+  camera.position.set(-200, 200, -200);
 
   const controls = new OrbitControls(camera, renderer.domElement);
   controls.target.set(0, 5, 0);
@@ -51,7 +61,18 @@ function main() {
   const scene = new THREE.Scene();
   scene.background = new THREE.Color('white');
 
-  {
+  const fillEdges = (maxX, maxY, maxZ) => {
+    const cubeSize = 1;
+    const cubeGeo = new THREE.BoxGeometry(maxX, maxY, maxZ);
+    const edges = new THREE.EdgesGeometry(cubeGeo);
+    const material = new THREE.LineBasicMaterial({ color: 0x0000ff, transparent: true, opacity: 0.05 });
+
+    const line = new THREE.LineSegments(edges, material);
+    line.position.set(maxX / 2, maxY / 2, maxZ / 2);
+    scene.add(line);
+  };
+
+  const addPlane = () => {
     const planeSize = 40;
 
     const loader = new THREE.TextureLoader();
@@ -70,37 +91,9 @@ function main() {
     });
     const mesh = new THREE.Mesh(planeGeo, planeMat);
     mesh.rotation.x = Math.PI * -0.5;
+    mesh.position.y = -0.01;
     scene.add(mesh);
-  }
-
-  {
-    const cubeSize = 4;
-    const cubeGeo = new THREE.BoxGeometry(cubeSize, cubeSize, cubeSize);
-
-    const cubeMat = new THREE.MeshBasicMaterial({ color: 'green', transparent: true, opacity: 0.3 });
-    const mesh = new THREE.Mesh(cubeGeo, cubeMat);
-    mesh.position.set(0, cubeSize / 2, cubeSize);
-    scene.add(mesh);
-
-    const edges = new THREE.EdgesGeometry(cubeGeo);
-    const line = new THREE.LineSegments(edges, new THREE.LineBasicMaterial({ color: 0xffffff }));
-    line.position.set(0, cubeSize / 2, cubeSize);
-    scene.add(line);
-  }
-
-  {
-    const cubeSize = 4;
-    const cubeGeo = new THREE.BoxGeometry(cubeSize, cubeSize, cubeSize);
-    const cubeMat = new THREE.MeshPhongMaterial({ color: 'green', transparent: true, opacity: 0.3 });
-    const mesh = new THREE.Mesh(cubeGeo, cubeMat);
-    mesh.position.set(0, cubeSize / 2, cubeSize * 2);
-    scene.add(mesh);
-
-    const edges = new THREE.EdgesGeometry(cubeGeo);
-    const line = new THREE.LineSegments(edges, new THREE.LineBasicMaterial({ color: 0xffffff }));
-    line.position.set(0, cubeSize / 2, cubeSize * 2);
-    scene.add(line);
-  }
+  };
 
   {
     const color = 0xffffff;
@@ -108,6 +101,16 @@ function main() {
     const light = new THREE.AmbientLight(color, intensity);
     scene.add(light);
   }
+
+  // addPlane();
+  fillEdges(180, 180, 60);
+  const c1 = getCube(0, 0, 0);
+  const c2 = getCube(1, 0, 0);
+  const c3 = getCube(2, 0, 0);
+
+  scene.add(c1);
+  scene.add(c2);
+  scene.add(c3);
 
   function render() {
     renderer.render(scene, camera);
